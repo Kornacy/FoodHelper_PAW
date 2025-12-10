7b8b77ad8b05a02c296ffc73c6a8490d5739f9f6
Kamil <kamilbuszta.3@gmail.com>
2025-12-10 23:03:58 +0100

    feat: create backend node server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const { sequelize } = require('./src/models'); 
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+async function startApp() {
+  try {
+    console.log("⏳ Łączenie z bazą danych...");
+
+    await sequelize.sync({ alter: true });
+    
+    console.log("✅ Sukces! Baza danych i tabele są gotowe.");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Serwer działa na porcie ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("❌ Błąd startu aplikacji:", error.message);
+    console.log("Podpowiedź: Upewnij się, że kontener Docker z bazą działa.");
+  }
+}
+
+startApp();
